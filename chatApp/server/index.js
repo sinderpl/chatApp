575ba583ed8e46c515fbdcd614e604503e5ed141
Alan <sinderpl@gmail.com>
2019-12-22 22:18:20 +0000

    Adding socket code

--- chatApp/server/index.js
+++ chatApp/server/index.js
@@ -1,11 +1,13 @@
 // app.js
-const express = require('express')
+const express = require('express');
+const io = require('socket.io')(80);
+const port = 3000;
 
 // Create Express app
-const app = express()
+const app = express();
 
 // A sample route
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 // Start the Express server
-app.listen(3000, () => console.log('Server running on port 3000!'))
+app.listen(port, () => console.log('Server running on port ', port));
